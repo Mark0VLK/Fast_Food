c94c593bd3b17265fe21e6b2f64ef4099f6c896c
Aliaksandr-78 <sasha-mv21634@yandex.by>
2023-10-13 16:32:04 +0300

    update

--- js/Login.js
+++ js/Login.js
@@ -4,24 +4,38 @@ const container = document.getElementById('container');
 const closeButtonLeft = document.getElementById('closeButtonLeft'); // Получаем кнопку закрытия слева
 const closeButtonRight = document.getElementById('closeButtonRight'); // Получаем кнопку закрытия справа
 const modal = document.querySelector('.modal-login');
+const overlay = document.getElementById('ModalOverlay');
 
 signUpButton.addEventListener('click', () => {
     container.classList.add("right-panel-active");
     modal.style.display = 'block';
+    document.body.classList.add('modal-active'); // Добавить класс для блокировки страницы
 });
 
 signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
     modal.style.display = 'block';
+    document.body.classList.add('modal-active'); // Добавить класс для блокировки страницы
 });
 
 // Добавляем слушатели событий для кнопок закрытия
 closeButtonLeft.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
     modal.style.display = 'none';
+    document.body.classList.remove('modal-active'); // Удалить класс для разблокировки страницы
 });
 
 closeButtonRight.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
     modal.style.display = 'none';
+    document.body.classList.remove('modal-active'); // Удалить класс для разблокировки страницы
 });
+
+overlay.addEventListener('click', (event) => {
+    if(event.target === overlay) { // Проверка, не произошло ли клик на самом модальном окне
+        modal.style.display = 'none';
+        container.classList.remove('right-panel-active');
+        document.body.classList.remove('modal-active');
+    }
+});
+
