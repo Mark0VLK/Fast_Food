1eaaa70e75aa107b1733c1991aaee2037dfd57a5
Aliaksandr-78 <sasha-mv21634@yandex.by>
2023-10-18 10:00:54 +0300

    add about us

--- js/Button.js
+++ js/Button.js
@@ -1,41 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const meinSection = document.getElementById('Mein');
+    const aboutSection = document.getElementById('About');
     const menuSection = document.getElementById('Menu');
     const basketSection = document.getElementById('Basket');
-    const contactsSection = document.getElementById('Contacts');
     const showMenuButton = document.getElementById('showMenuButton');
-    const showMeinButton = document.getElementById('showMeinButton');
+    const showAboutButton = document.getElementById('showAboutButton');
     const showBasketButton = document.getElementById('showBasketButton');
-    const showContactsButton = document.getElementById('showContactsButton');
 
-    function showMein() {
+    function showAbout() {
         basketSection.style.display = 'none';
-        contactsSection.style.display = 'none';
         menuSection.style.display = 'none';
-        meinSection.style.display = 'block';
+        aboutSection.style.display = 'block';
     }
 
     function showMenu() {
         basketSection.style.display = 'none';
-        contactsSection.style.display = 'none';
-        meinSection.style.display = 'none';
+        aboutSection.style.display = 'none';
         menuSection.style.display = 'block';
     }
     function showBasket() {
-        contactsSection.style.display = 'none';
-        meinSection.style.display = 'none';
+        aboutSection.style.display = 'none';
         menuSection.style.display = 'none';
         basketSection.style.display = 'block';
     }
-    function showContacts() {
-        basketSection.style.display = 'none';
-        meinSection.style.display = 'none';
-        menuSection.style.display = 'none';
-        contactsSection.style.display = 'block';
-    }
 
     showMenuButton.addEventListener('click', showMenu);
-    showMeinButton.addEventListener('click', showMein);
+    showAboutButton.addEventListener('click', showAbout);
     showBasketButton.addEventListener('click', showBasket)
-    showContactsButton.addEventListener('click', showContacts)
 });
